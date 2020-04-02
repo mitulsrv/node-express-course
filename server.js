d03b82a52667a079ea0a9fcf6e55a45ba7bda73a
Mitul Sarvaiya <mitulsarvaiya9500@gmail.com>
2020-04-02 11:50:51 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 
+const mockUserData = [
+    {name: "Mitul"},
+    {name: "Rajan"}
+]
+
+
+app.get('/users', (req, res) => {
+    res.json({
+        success:true,
+        message: "successfully got users, Nice!",
+        users: mockUserData
+    })
+})
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
