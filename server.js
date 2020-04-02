01b36416917407b9c93cc8af22aae2049f9bfafc
Mitul Sarvaiya <mitulsarvaiya9500@gmail.com>
2020-04-02 12:00:21 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json());
+
 
 const mockUserData = [
     {name: "Mitul"},
@@ -22,6 +27,29 @@ app.get('/users/:id', (req, res)=>{
         message: "successfully got users, Nice!",
         users: req.params.id
     })
+})
+
+
+app.post('/users', (req, res) =>{
+    const username = req.body.username
+    const password = req.body.password
+
+    const foUsername = 'mitulsrv'
+    const foPassword = 'mitulsrv'
+
+    if(username === foUsername && password === foPassword){
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    }else{
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+
 })
 
 
